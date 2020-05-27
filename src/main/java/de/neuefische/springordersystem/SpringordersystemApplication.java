3478fbaa9b363202cfa083896cf632fc36a76814
Thanh <Thanh.Duong2503@gmx.de>
2020-05-27 10:03:02 +0200

    initialcommit

---
+++ src/main/java/de/neuefische/springordersystem/SpringordersystemApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.springordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringordersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringordersystemApplication.class, args);
+    }
+
+}
