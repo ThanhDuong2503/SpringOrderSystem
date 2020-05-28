904e969800d1856832422fc53925173e0c00c2d3
Thanh <Thanh.Duong2503@gmx.de>
2020-05-28 16:16:39 +0200

    testcommit

--- src/test/java/de/neuefische/springordersystem/Controller/OrderControllerTest.java
+++ src/test/java/de/neuefische/springordersystem/Controller/OrderControllerTest.java
@@ -40,52 +40,52 @@ class OrderControllerTest {
         orderDb.clearDb();
     }
 
-//    @Test
-//    public void listOrdersShouldReturnAllOrders() {
-//
-//        // GIVEN
-//
-//        // WHEN
-//        ResponseEntity<OrderController[]> response = restTemplate.getForEntity("http://localhost:" + port + "/orders", OrderController[].class);
-//        HttpStatus statusCode = response.getStatusCode();
-//        OrderController[] orderControllers = response.getBody();
-//
-//        // THEN
-//        assertEquals(HttpStatus.OK, statusCode);
-//        assertEquals(0, orderControllers.length);
-//    }
-//
-//    @Test
-//    public void orderProductsShouldReturnOrderedProducts() {
-//        // POST Process ; platziert order
-//        ArrayList<String> productId = new ArrayList<>();
-//        ResponseEntity<Order>postResponse = restTemplate.postForEntity("http://localhost:" + port + "/orders", productId, Order.class);
-//        assertEquals(HttpStatus.OK, postResponse.getStatusCode());
-//
-//        // WHEN Get Process; ruft geordete Order auf
-//        ResponseEntity<Order[]> response = restTemplate.getForEntity("http://localhost:" + port + "/orders", Order[].class);
-//        HttpStatus statusCode = response.getStatusCode();
-//        Order[] orders = response.getBody();
-//
-//        // THEN
-//        assertEquals(HttpStatus.OK, statusCode);
-//        assertEquals(1,orders.length);
-//    }
-//
-//    @Test
-//    public void listProductsShouldReturnAllProducts() {
-//
-//        // GIVEN
-//
-//        // WHEN
-//        ResponseEntity<Product[]> response = restTemplate.getForEntity("http://localhost:" + port + "/products", Product[].class);
-//        HttpStatus statusCode = response.getStatusCode();
-//        Product[] products = response.getBody();
-//
-//        // THEN
-//        assertEquals(HttpStatus.OK, statusCode);
-//        assertEquals(5, products.length);
-//    }
+    @Test
+    public void listOrdersShouldReturnAllOrders() {
+
+        // GIVEN
+
+        // WHEN
+        ResponseEntity<OrderController[]> response = restTemplate.getForEntity("http://localhost:" + port + "/orders", OrderController[].class);
+        HttpStatus statusCode = response.getStatusCode();
+        OrderController[] orderControllers = response.getBody();
+
+        // THEN
+        assertEquals(HttpStatus.OK, statusCode);
+        assertEquals(0, orderControllers.length);
+    }
+
+    @Test
+    public void orderProductsShouldReturnOrderedProducts() {
+        // POST Process ; platziert order
+        ArrayList<String> productId = new ArrayList<>();
+        ResponseEntity<Order>postResponse = restTemplate.postForEntity("http://localhost:" + port + "/orders", productId, Order.class);
+        assertEquals(HttpStatus.OK, postResponse.getStatusCode());
+
+        // WHEN Get Process; ruft geordete Order auf
+        ResponseEntity<Order[]> response = restTemplate.getForEntity("http://localhost:" + port + "/orders", Order[].class);
+        HttpStatus statusCode = response.getStatusCode();
+        Order[] orders = response.getBody();
+
+        // THEN
+        assertEquals(HttpStatus.OK, statusCode);
+        assertEquals(1,orders.length);
+    }
+
+    @Test
+    public void listProductsShouldReturnAllProducts() {
+
+        // GIVEN
+
+        // WHEN
+        ResponseEntity<Product[]> response = restTemplate.getForEntity("http://localhost:" + port + "/products", Product[].class);
+        HttpStatus statusCode = response.getStatusCode();
+        Product[] products = response.getBody();
+
+        // THEN
+        assertEquals(HttpStatus.OK, statusCode);
+        assertEquals(5, products.length);
+    }
 
     @Test
     public void orderProductsShouldAddOrderToDatabase() {
